a520639a7675de19e7141d20c7bcf766cc3a122c
arthurtilly <32559225+arthurtilly@users.noreply.github.com>
2023-04-20 04:14:02 +1200

    Audio data cleanup (#51)
    * rename sfx to samples
    
    * move samples into rodata section of single file
    
    * clean up audio data a bit

--- audio/instruments.c
+++ audio/instruments.c
@@ -1,8 +1,6 @@
 #include "global.h"
 #include "sound.h"
 
-#include "instruments.inc.c"
+#include "instrument_data.inc.c"
 #include "instrument_banks.inc.c"
 #include "instrument_bank_list.inc.c"
-#include "sequence_names.inc.c"
-#include "sequence_data.inc.c"
