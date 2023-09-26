5de1988bd9d23536d8e8cbc97b93acb539a326f8
EstexNT <66506310+EstexNT@users.noreply.github.com>
2023-09-26 20:20:47 +0300

    Gyro/Rumble code decompilation (#60)
    * Decompile most functions
    
    * Decompile the rest of the functions
    
    * Small changes
    
    * Decompile some gyro/rumble interface functions
    
    * Finish decompiling code_080092cc
    
    * Clean up

--- src/lib_0804e564.c
+++ src/lib_0804e564.c
@@ -2,32 +2,212 @@
 
 asm(".include \"include/gba.inc\"");//Temporary
 
-#include "asm/lib_0804e564/asm_0804e564.s"
+// Initialise GPIO
+void func_0804e564(void) {
+    D_03004438 = 2 | 1;
+    *D_08bd0cc8 = D_03004438;
+    D_0300443a = 2 | 1;
+    D_0300443a |= 8;
+    *D_08bd0ccc = D_0300443a;
+}
 
-#include "asm/lib_0804e564/asm_0804e598.s"
+// Read Gyro Data
+s32 func_0804e598(void) {
+    volatile s32 gyroData;
+    volatile s32 (*readFunc)(volatile u16 *) = func_0804e938;
 
-#include "asm/lib_0804e564/asm_0804e618.s"
+    *D_08bd0cd0 = 1;
+    D_03004438 &= ~1;
+    *D_08bd0cc8 = D_03004438;
 
-#include "asm/lib_0804e564/asm_0804e640.s"
+    gyroData = readFunc(&D_03004438);
+    D_03004438 |= 1;
+    *D_08bd0cc8 = D_03004438;
+    *D_08bd0cd0 = 0;
 
-#include "asm/lib_0804e564/asm_0804e690.s"
+    if ((gyroData != 0) && (gyroData != 0xfff)) {
+        return gyroData << 4;
+    } else {
+        return -1;
+    }
+}
 
-#include "asm/lib_0804e564/asm_0804e6c4.s"
+// Enable Rumble
+void func_0804e618(u32 arg0) {
+    D_03004438 &= ~8;
+    D_03004438 |= (arg0 << 3);
+    *D_08bd0cc8 = D_03004438;
+}
 
-#include "asm/lib_0804e564/asm_0804e6e4.s"
+// Initialise Gyro/Rumble
+void func_0804e640(struct struct_0300443c *arg0) {
+    func_0804e564();
+    D_0300443c = arg0;
 
-#include "asm/lib_0804e564/asm_0804e77c.s"
+    arg0->unk0 = 0;
+    arg0->unk20 = D_08bd0cd4[0];
+    arg0->unk8 = 0x8000;
+    arg0->unkC = arg0->unk8 << arg0->unk20;
+    arg0->unk22 = 0;
+    arg0->unk1C = 0;
+    D_030064d4 = func_0804e598;
+    D_03004440 = TRUE;
+}
 
-#include "asm/lib_0804e564/asm_0804e834.s"
+void func_0804e690(u32 arg0) {
+    u8 temp;
 
-#include "asm/lib_0804e564/asm_0804e870.s"
+    D_0300443c->unk0 = arg0;
+    temp = D_08bd0cd4[(arg0 / 8) % 4];
+    D_0300443c->unkC = (D_0300443c->unkC >> D_0300443c->unk20) << temp;
+    D_0300443c->unk20 = temp;
+}
 
-#include "asm/lib_0804e564/asm_0804e8bc.s"
+void func_0804e6c4(u32 arg0) {
+    D_0300443c->unk10 = 0;
+    D_0300443c->unk12 = 0;
+    D_0300443c->unk18 = 0x7fffffff;
+    D_0300443c->unk14 = 0;
+    D_0300443c->unk1C = 0;
+    D_0300443c->unk22 = arg0;
+}
 
-#include "asm/lib_0804e564/asm_0804e8cc.s"
+void func_0804e6e4(void) {
+    s32 temp_r1;
+    u32 temp_r3;
+    
+    if (!D_0300443c->unk22) return;
+    
+    temp_r1 = D_0300443c->unk4;
+    if (temp_r1 < D_0300443c->unk18) {
+        D_0300443c->unk18 = temp_r1;
+    }
+    if (temp_r1 > D_0300443c->unk14) {
+        D_0300443c->unk14 = temp_r1;
+    }
+    temp_r3 = D_0300443c->unk1C + temp_r1;
+    D_0300443c->unk1C = temp_r3;
+    D_0300443c->unk10++;
+    if ((D_0300443c->unk14 - D_0300443c->unk18) < 0x28) {
+        if (0x1d < D_0300443c->unk10) {
+            D_0300443c->unk8 = temp_r3 / D_0300443c->unk10;
+            D_0300443c->unk22 = 0;
+            D_0300443c->unk10 = 0;
+            D_0300443c->unk18 = 0x7fffffff;
+            D_0300443c->unk14 = 0;
+            D_0300443c->unk1C = 0;
+            return;
+        }
+    } else {
+        if (D_0300443c->unk12 < D_0300443c->unk10) {
+            D_0300443c->unk12 = D_0300443c->unk10;
+            D_0300443c->unk8 = temp_r3 / D_0300443c->unk10;
+        }
+        D_0300443c->unk10 = 0;
+        D_0300443c->unk18 = 0x7fffffff;
+        D_0300443c->unk14 = 0;
+        D_0300443c->unk1C = 0;
+    }
+        D_0300443c->unk22--;
+}
 
-#include "asm/lib_0804e564/asm_0804e8f8.s"
+void func_0804e77c(void) {
+    u32 temp_r7 = TRUE;
+    u32 temp_r5;
+    u32 temp_r4;
+    u32 temp_r1;
+    u32 temp_r0;
+    s32 temp_r0_1;
+    
+    if ((D_0300443c->unk0 & 0x20) || (D_0300443c->unk22)) return;
 
-#include "asm/lib_0804e564/asm_0804e914.s"
+    temp_r5 = D_0300443c->unk8;
+    temp_r1 = D_0300443c->unk4;
+    if (temp_r1 < D_0300443c->unk18) {
+        D_0300443c->unk18 = temp_r1;
+    }
+    if (temp_r1 > D_0300443c->unk14) {
+        D_0300443c->unk14 = temp_r1;
+    }
+    temp_r4 = D_0300443c->unk1C + temp_r1;
+    D_0300443c->unk1C = temp_r4;
+    D_0300443c->unk10++;
+    if ((D_0300443c->unk14 - D_0300443c->unk18) < 0x30) {
+        if (D_0300443c->unk10 > 0x1d) {
+            temp_r4 /= D_0300443c->unk10;
+            temp_r0_1 = temp_r4 - temp_r5;
+            temp_r0 = 0x100 / (u32)(clamp_int32(ABS(temp_r0_1), 0x10, 0x40));
+            if (temp_r4 < temp_r5) {
+                temp_r5 -= temp_r0;
+                if (temp_r5 < temp_r4) {
+                    temp_r5 = temp_r4;
+                }
+            }
+            if (temp_r4 > temp_r5) {
+                temp_r5 += temp_r0;
+                if (temp_r5 > temp_r4) {
+                    temp_r5 = temp_r4;
+                }
+            }
+        } else {
+            temp_r7 = FALSE;
+        }
+    }
+    if (temp_r7) {
+        D_0300443c->unk10 = 0;
+        D_0300443c->unk18 = 0x7fffffff;
+        D_0300443c->unk14 = 0;
+        D_0300443c->unk1C = 0;
+    }
+    D_0300443c->unk8 = temp_r5;
+}
 
-#include "asm/lib_0804e564/asm_0804e92c.s"
+u32 func_0804e834(void) {
+    if (D_0300443c->unk4 != -1) {
+        D_0300443c->unkC = (D_0300443c->unkC - (D_0300443c->unkC >> D_0300443c->unk20)) + D_0300443c->unk4;
+    }
+    return (D_0300443c->unkC >> D_0300443c->unk20) - D_0300443c->unk8;
+}
+
+u32 func_0804e870(u32 *arg0) {
+    D_0300443c->unk4 = D_030064d4();
+    if (D_0300443c->unk4 != -1) {
+        func_0804e6e4();
+        func_0804e77c();
+    }
+    *arg0 = func_0804e834();
+    if (D_0300443c->unk0 & 1) {
+        *arg0 = -*arg0;
+    }
+    return D_0300443c->unk22;
+}
+
+void func_0804e8bc(s32 *arg0) {
+    *arg0 = func_0804e598();
+}
+
+void func_0804e8cc(u32 arg0) {
+    if (arg0) {
+        D_0300443c->unk0 &= ~0x20;
+    } else {
+        D_0300443c->unk0 |= 0x20;
+    }
+}
+
+// Set Rumble
+void func_0804e8f8(u32 arg0) {
+    if (!D_03004440) {
+        arg0 = FALSE;
+    }
+    func_0804e618(arg0);
+}
+
+// Turn off current Rumble, toggle Rumble
+void func_0804e914(u32 arg0) {
+    func_0804e618(0);
+    D_03004440 = arg0;
+}
+
+u32 func_0804e92c(void) {
+    return D_0300443c->unk8;
+}
--- src/lib_0804e564.h
+++ src/lib_0804e564.h
@@ -2,18 +2,55 @@
 
 #include "global.h"
 
-// extern ? func_0804e564(?);
-// extern ? func_0804e598(?);
-// extern ? func_0804e618(?);
-// extern ? func_0804e640(?);
-// extern ? func_0804e690(?);
-// extern ? func_0804e6c4(?);
-// extern ? func_0804e6e4(?);
-// extern ? func_0804e77c(?);
-// extern ? func_0804e834(?);
-// extern ? func_0804e870(?);
-// extern ? func_0804e8bc(?);
-// extern ? func_0804e8cc(?);
-// extern ? func_0804e8f8(?);
-// extern ? func_0804e914(?);
-// extern ? func_0804e92c(?);
+// Gyro/Rumble Library
+
+struct struct_0300443c { // Gyro Status?
+    u32 unk0;
+    s32 unk4;
+    u32 unk8;
+    s32 unkC;
+    u16 unk10;
+    u16 unk12;
+    u32 unk14;
+    u32 unk18;
+    u32 unk1C;
+    u8 unk20;
+    u8 unk21;
+    u16 unk22;
+};
+
+#define REG_GPIO_DATA *(volatile u16 *)(GameROMBase + 0xc4)
+#define REG_GPIO_DIR *(volatile u16 *)(GameROMBase + 0xc6)
+#define REG_GPIO_CNT *(volatile u16 *)(GameROMBase + 0xc8)
+
+extern volatile u16 D_03004438; // GPIO Data Reserve
+extern volatile u16 D_0300443a; // GPIO Direction Reserve
+extern struct struct_0300443c *D_0300443c; // Gyro Status
+extern u8 D_03004440; // Rumble Enabled
+
+extern s32 (*D_030064d4)(void); // Read Gyro Data Function?
+
+extern volatile u16 *D_08bd0cc8; // GPIO Data Pointer
+extern volatile u16 *D_08bd0ccc; // GPIO Direction Pointer
+extern volatile u16 *D_08bd0cd0; // GPIO Control Pointer
+extern u8 D_08bd0cd4[4];
+
+
+extern void func_0804e564(void); // Initialise GPIO
+extern s32 func_0804e598(void); // Read Gyro Data
+extern void func_0804e618(u32); // Enable Rumble
+extern void func_0804e640(struct struct_0300443c *); // Initialise Gyro/Rumbl
+extern void func_0804e690(u32);
+extern void func_0804e6c4(u32);
+extern void func_0804e6e4(void);
+extern void func_0804e77c(void);
+extern u32 func_0804e834(void);
+extern u32 func_0804e870(u32 *);
+extern void func_0804e8bc(s32 *);
+extern void func_0804e8cc(u32);
+extern void func_0804e8f8(u32); // Toggle Rumble
+extern void func_0804e914(u32); // Turn off current Rumble, toggle Rumble
+extern u32 func_0804e92c(void);
+
+// ARM 
+extern volatile s32 func_0804e938(volatile u16 *);
