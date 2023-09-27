0f7b3f2bb30d566c45a892e45d1e9ff178816699
genericarrangements <genericarrangements.wah@gmail.com>
2023-09-27 18:53:04 +1000

    Decompile many SoundPlayer-related functions.

---
+++ src/midi_lfo.inc.c
@@ -0,0 +1,14 @@
+
+
+/* AUDIO LIBRARY - LOW-FREQUENCY OSCILLATOR */
+
+
+#include "asm/lib_08049144/asm_0804ae1c.s"
+
+#include "asm/lib_08049144/asm_0804ae54.s"
+
+#include "asm/lib_08049144/asm_0804ae60.s"
+
+#include "asm/lib_08049144/asm_0804ae6c.s"
+
+#include "asm/lib_08049144/asm_0804af0c.s"
---
+++ src/midi_psg.inc.c
@@ -0,0 +1,16 @@
+
+
+/* AUDIO LIBRARY - PSG SOUND CHANNELS */
+
+
+#include "asm/lib_08049144/asm_0804af30.s"
+
+#include "asm/lib_08049144/asm_0804af74.s"
+
+#include "asm/lib_08049144/asm_0804afa4.s"
+
+#include "asm/lib_08049144/asm_0804afd8.s"
+
+#include "asm/lib_08049144/asm_0804aff0.s"
+
+#include "asm/lib_08049144/asm_0804b2c4.s"
