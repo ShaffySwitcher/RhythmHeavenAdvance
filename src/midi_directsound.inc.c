222001e3a24d793ef144c9bba0419ba2b89720ac
genericarrangements <genericarrangements.wah@gmail.com>
2023-09-30 11:18:53 +1000

    Decompile MIDI library initialisation function. Adjust documentation for some MIDI sequence-related functions. Delete unneeded ASM.

--- src/midi_directsound.inc.c
+++ src/midi_directsound.inc.c
@@ -3,40 +3,77 @@
 /* AUDIO LIBRARY - DIRECTSOUND */
 
 
+// INTERRUPT_DMA2
 #include "asm/lib_08049144/asm_08049144.s"
 
+
+// Initialise SampleStream
 #include "asm/lib_08049144/asm_0804930c.s"
 
+
+// Reset SampleStream
 #include "asm/lib_08049144/asm_08049394.s"
 
+
+// Close SampleStream
 #include "asm/lib_08049144/asm_080493b0.s"
 
+
+// Set SampleStream Panning
 #include "asm/lib_08049144/asm_080493c8.s"
 
+
+// Set SampleStream Volume Envelope
 #include "asm/lib_08049144/asm_080493e4.s"
 
+
+// Set SampleStream Frequency Envelope
 #include "asm/lib_08049144/asm_080493f4.s"
 
+
+// Set SampleStream unk0_b2
 #include "asm/lib_08049144/asm_08049450.s"
 
+
+// Set SampleStream unk0_b3
 #include "asm/lib_08049144/asm_08049470.s"
 
+
+// Initialise DirectSound
 #include "asm/lib_08049144/asm_08049490.s"
 
+
+// Update DirectSound
 #include "asm/lib_08049144/asm_080497f8.s"
 
+
+// Initialise(?) REG_DMA1CNT & REG_DMA2CNT (unused)
 #include "asm/lib_08049144/asm_08049ad8.s"
 
+
+// Set Reverb Controllers
 #include "asm/lib_08049144/asm_08049b34.s"
 
+
+// Check If Given SampleStream Active
 #include "asm/lib_08049144/asm_08049b5c.s"
 
+
+// Set Filter EQ Position
 #include "asm/lib_08049144/asm_08049b70.s"
 
+
+// Set Filter EQ High Gain
 #include "asm/lib_08049144/asm_08049b8c.s"
 
+
+// Initialise Filter EQ
 #include "asm/lib_08049144/asm_08049bac.s"
 
+
+// Reset Filter EQ
 #include "asm/lib_08049144/asm_08049be4.s"
 
+
+// Set Filter EQ
 #include "asm/lib_08049144/asm_08049bfc.s"
