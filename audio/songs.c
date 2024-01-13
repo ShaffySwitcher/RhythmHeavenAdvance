3f4cdba37c3f00558920761ee91a83df2dbe29fa
genericarrangements <genericarrangements.wah@gmail.com>
2024-01-14 10:31:19 +1100

    Generate SongList and SongTable using a singular array definition.

--- audio/songs.c
+++ audio/songs.c
@@ -1,7 +1,17 @@
 #include "global.h"
 #include "sound.h"
 
+// Song Titles
 #include "song_titles.inc.c"
+
+// Song Headers
 #include "song_headers.inc.c"
-#include "song_list.inc.c"
-#include "song_table.inc.c"
+
+// Song List (Unused)
+u32 D_08a9e8e0 = 1924 - 1; // Size of the Song List - 1 (Unused)
+#include "song_array_gen.inc.c"
+
+// Song Table
+u32 D_08aa06f4 = 1924; // Size of the Song Table (Unused)
+#define GEN_SONG_TABLE
+#include "song_array_gen.inc.c"
