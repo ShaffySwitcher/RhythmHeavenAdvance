002c7a42e9ef5a6ac8a6884e4133f37a19ab2335
GENERIC <52912884+GenericArrangements@users.noreply.github.com>
2025-02-21 07:22:06 +1100

    Decompile Bouncy Road (#76)
    * Decompile all of Bouncy Road. Delete unneeded ASM.
    
    * Rename unlabelled Bouncy Road functions and scripts. Add a few more macros.
    
    * Adjust number format of time-related cue data for Bouncy Road.
    
    * Specify in description of FIXED_POINT_MUL() that both parameters are fixed-point.

--- asm/engines/bouncy_road/asm_0802e538.s
+++
@@ -1,8 +0,0 @@
-asm(".syntax unified \n\
-\n\
-.balign 4, 0 \n\
-\n\
-thumb_func_start bouncy_road_engine_event_stub \n\
-/* 0802e538 */ BX LR \n\
-.balign 4, 0 \n\
-.syntax divided");
--- include/global.h
+++ include/global.h
@@ -26,13 +26,19 @@ typedef enum Boolean {
 
 #define lerp(start, end, t, total) ((start) + fast_divsi3(((end) - (start)) * (t), (total)))
 
+// Q24.8 fixed-point types.
 typedef u16 u8_8;
 typedef u32 u24_8;
 typedef s16 s8_8;
 typedef s32 s24_8;
+// Convert integer to fixed-point value.
 #define INT_TO_FIXED(x) ((s32)((x) * 256))
+// Convert fixed-point value to integer.
 #define FIXED_TO_INT(x) ((s32)((x) >> 8))
+// Multiply two fixed-point values.
 #define FIXED_POINT_MUL(a, b) (((a) * (b)) >> 8)
+// Divide numbers, where 'd' is always fixed-point.
+#define FIXED_POINT_DIV(n, d) (((n) << 8) / (d))
 
 #define ARRAY_COUNT(a) (s32)(sizeof(a))/sizeof((a)[0])
 
