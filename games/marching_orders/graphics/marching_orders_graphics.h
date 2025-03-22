348c61accce25952b087fa6366265aea5f30f054
itaific <itaific@gmail.com>
2025-03-22 22:12:02 +0200

    marching arders? why did i call it ma?

--- games/marching_orders/graphics/marching_orders_graphics.h
+++ games/marching_orders/graphics/marching_orders_graphics.h
@@ -4,8 +4,8 @@
 #include "graphics.h"
 
 /* MARCHING ORDERS - ANIMATIONS */
-extern const u8 ma2_bin;
-extern const u8 ma_bin;
+extern const u8 mo2_bin;
+extern const u8 mo_bin;
 
 extern struct Animation anim_marching1_test[];
 extern struct Animation anim_marching1_turn_l[];
