975e37a66ddf8111ffd0ab1ab86dcccb112dfa67
itaific <itaific@gmail.com>
2025-03-29 21:35:29 +0300

    number sprite change

--- src/engines/quiz_show.c
+++ src/engines/quiz_show.c
@@ -25,12 +25,12 @@ void func_08037178(void) {
     sprite_create(gSpriteHandler, &anim_quiz_show_high_score_label, 0, 0x3a, 4, 0x800, 0, 0, 0);
 
     for (i = 0; i < 2; i++) {
-        gQuizShow->curScoreSprites[i] = sprite_create(gSpriteHandler, &anim_quiz_show_score_num, 0, 0xc0 - (i*8), 4, 0x800, 0, 0x7f, 0);
+        gQuizShow->curScoreSprites[i] = sprite_create(gSpriteHandler, &anim_quiz_show_score_num, 0, 0xC6 - (i*8), 4, 0x800, 0, 0x7f, 0);
     }
 
     
     for (i = 0; i < 2; i++) {
-        gQuizShow->highScoreSprites[i] = sprite_create(gSpriteHandler, &anim_quiz_show_score_num, 0, 0x3a - (i*8), 4, 0x800, 0, 0x7f, 0);
+        gQuizShow->highScoreSprites[i] = sprite_create(gSpriteHandler, &anim_quiz_show_score_num, 0, 0x27 - (i*8), 4, 0x800, 0, 0x7f, 0);
     }
     
     func_0803709c();
