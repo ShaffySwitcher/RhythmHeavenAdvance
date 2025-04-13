3e4368837bec26201b7ff29b8945d0166590f932
Nibbl-z <93560873+Nibbl-z@users.noreply.github.com>
2025-04-13 00:36:05 -0600

    Decompile Staff Credit (#80)
    * decompile every function :P
    
    * name/document all struct values, functions, variables, and beatscript events
    
    * move all staff credit graphics from graphics/unknown to games/staff_credit/graphics

--- asm/engines/staff_credit/asm_08023990.s
+++
@@ -1,8 +0,0 @@
-asm(".syntax unified \n\
-\n\
-.balign 4, 0 \n\
-\n\
-thumb_func_start staff_credit_input_event \n\
-/* 08023990 */ BX LR \n\
-.balign 4, 0 \n\
-.syntax divided");
--- asm/engines/staff_credit/asm_08023994.s
+++
@@ -1,8 +0,0 @@
-asm(".syntax unified \n\
-\n\
-.balign 4, 0 \n\
-\n\
-thumb_func_start staff_credit_common_beat_animation \n\
-/* 08023994 */ BX LR \n\
-.balign 4, 0 \n\
-.syntax divided");
