f0b4ce37e1606a8de80b19a98a84e94d12d96c22
arthurtilly <arthurtilly413@gmail.com>
2025-05-05 02:19:40 +1200

    drum live

--- games/drum_live/graphics/drum_live_menu_graphics.h
+++ games/drum_live/graphics/drum_live_menu_graphics.h
@@ -12,7 +12,7 @@ extern struct Animation anim_drum_live_menu_poster[];
 
 /* CONCERT HALL - COMPRESSED GRAPHICS */
 
-extern struct CompressedGraphics D_08c79bb0;
+extern struct CompressedGraphics drum_live_menu_graphics_08c79bb0;
 
 /* CONCERT HALL - PALETTES */
 
