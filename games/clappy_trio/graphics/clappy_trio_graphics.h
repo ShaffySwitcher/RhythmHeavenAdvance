40010a9c304533a36474820654803dd1881e43cd
arthurtilly <arthurtilly413@gmail.com>
2025-05-06 08:43:42 +1200

    clappy trio labels

--- games/clappy_trio/graphics/clappy_trio_graphics.h
+++ games/clappy_trio/graphics/clappy_trio_graphics.h
@@ -19,9 +19,9 @@ extern struct Animation anim_clappy_trio_text_box[];
 
 /* THE CLAPPY TRIO - COMPRESSED GRAPHICS */
 
-extern struct CompressedGraphics clappy_trio_graphics_08c61e18; // BG Tiles - The Clappy Trio
-extern struct CompressedGraphics clappy_trio_graphics_08c6214c; // BG Map - The Clappy Trio
-extern struct CompressedGraphics clappy_trio_graphics_08c61654; // OBJ Tiles - The Clappy Trio
+extern struct CompressedGraphics clappy_trio_bg_tiles; // BG Tiles - The Clappy Trio
+extern struct CompressedGraphics clappy_trio_bg_map; // BG Map - The Clappy Trio
+extern struct CompressedGraphics clappy_trio_obj; // OBJ Tiles - The Clappy Trio
 
 /* THE CLAPPY TRIO - PALETTES */
 
@@ -44,9 +44,9 @@ extern struct Animation anim_snappy_trio_text_box[];
 
 /* THE SNAPPY TRIO - COMPRESSED GRAPHICS */
 
-extern struct CompressedGraphics clappy_trio_graphics_08c635bc; // BG Tiles - The Snappy Trio
-extern struct CompressedGraphics clappy_trio_graphics_08c638e0; // BG Map - The Snappy Trio
-extern struct CompressedGraphics clappy_trio_graphics_08c62d18; // OBJ Tiles - The Snappy Trio
+extern struct CompressedGraphics snappy_trio_bg_tiles; // BG Tiles - The Snappy Trio
+extern struct CompressedGraphics snappy_trio_bg_map; // BG Map - The Snappy Trio
+extern struct CompressedGraphics snappy_trio_obj; // OBJ Tiles - The Snappy Trio
 
 /* THE SNAPPY TRIO - PALETTES */
 
--- games/clappy_trio/graphics/clappy_trio_prologue_graphics.h
+++ games/clappy_trio/graphics/clappy_trio_prologue_graphics.h
@@ -9,9 +9,9 @@ extern struct Animation anim_clappy_trio_prologue_title[];
 
 /* PROLOGUE (THE CLAPPY TRIO) - COMPRESSED GRAPHICS */
 
-extern struct CompressedGraphics clappy_trio_prologue_graphics_08bf2f64;
-extern struct CompressedGraphics clappy_trio_prologue_graphics_08bf33cc;
-extern struct CompressedGraphics clappy_trio_prologue_graphics_08bf16b4;
+extern struct CompressedGraphics clappy_trio_prologue_bg_tiles;
+extern struct CompressedGraphics clappy_trio_prologue_bg_map;
+extern struct CompressedGraphics clappy_trio_prologue_obj;
 
 /* PROLOGUE (THE CLAPPY TRIO) - PALETTES */
 
@@ -24,9 +24,9 @@ extern struct Animation anim_snappy_trio_prologue_title[];
 
 /* PROLOGUE (THE SNAPPY TRIO) - COMPRESSED GRAPHICS */
 
-extern struct CompressedGraphics clappy_trio_prologue_graphics_08bf5ce8;
-extern struct CompressedGraphics clappy_trio_prologue_graphics_08bf60c8;
-extern struct CompressedGraphics clappy_trio_prologue_graphics_08bf374c;
+extern struct CompressedGraphics snappy_trio_prologue_bg_tiles;
+extern struct CompressedGraphics snappy_trio_prologue_bg_map;
+extern struct CompressedGraphics snappy_trio_prologue_obj;
 
 /* PROLOGUE (THE SNAPPY TRIO) - PALETTES */
 
