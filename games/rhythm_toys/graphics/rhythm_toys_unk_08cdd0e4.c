dcd6cad7cfe9876ae7ab9abcbb8087bd79cc4c3a
arthurtilly <arthurtilly413@gmail.com>
2025-05-05 18:40:06 +1200

    split out rhythm toys extra data

---
+++ games/rhythm_toys/graphics/rhythm_toys_unk_08cdd0e4.c
@@ -0,0 +1,15 @@
+#include "global.h"
+
+struct Vector2 D_08cdd0e4 = {200, 148};
+struct Vector2 D_08cdd0e8 = {124, 69};
+struct Vector2 D_08cdd0ec = {124, 69};
+struct Vector2 D_08cdd0f0 = {166, 80};
+struct Vector2 D_08cdd0f4 = {140, 100};
+struct Vector2 D_08cdd0f8 = {88, 96};
+struct Vector2 D_08cdd0fc = {79, 92};
+struct Vector2 D_08cdd100 = {80, 90};
+struct Vector2 D_08cdd104 = {86, 97};
+struct Vector2 D_08cdd108 = {125, 70};
+struct Vector2 D_08cdd10c = {64, 64};
+struct Vector2 D_08cdd110 = {64, 64};
+struct Vector2 D_08cdd114 = {64, 64};
