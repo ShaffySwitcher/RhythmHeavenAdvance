89777b31c6b5863bff87e9f5291c5a85377bef4f
arthurtilly <arthurtilly413@gmail.com>
2025-05-05 18:40:06 +1200

    split out rhythm toys extra data

--- games/rhythm_toys/graphics/rhythm_toys_graphics_08cda118.c
+++ games/rhythm_toys/graphics/rhythm_toys_graphics_08cda118.c
@@ -40,14 +40,3 @@ struct CompressedGraphics D_08cda118 = {
 	/* RLE Offset */		0xe00,
 	/* Double Compressed */	TRUE,
 };
-
-struct Vector2 D_08cda128 = {200, 148};
-struct Vector2 D_08cda12c = {120, 96};
-struct Vector2 D_08cda130 = {120, 96};
-struct Vector2 D_08cda134 = {171, 108};
-struct Vector2 D_08cda138 = {153, 123};
-struct Vector2 D_08cda13c = {80, 114};
-struct Vector2 D_08cda140 = {80, 114};
-struct Vector2 D_08cda144 = {80, 114};
-struct Vector2 D_08cda148 = {80, 114};
-
