d14229bbd362bfe36f8a6050796cc8d4bc186a80
arthurtilly <arthurtilly413@gmail.com>
2025-05-05 19:24:48 +1200

    wizards waltz

--- games/wizards_waltz/graphics/wizards_waltz_prologue_graphics.h
+++ games/wizards_waltz/graphics/wizards_waltz_prologue_graphics.h
@@ -9,9 +9,9 @@ extern struct Animation anim_wizards_waltz_prologue_title[];
 
 /* PROLOGUE (WIZARD'S WALTZ) - COMPRESSED GRAPHICS */
 
-extern struct CompressedGraphics D_08c339dc;
-extern struct CompressedGraphics D_08c33c14;
-extern struct CompressedGraphics D_08c33640;
+extern struct CompressedGraphics wizards_waltz_prologue_graphics_08c339dc;
+extern struct CompressedGraphics wizards_waltz_prologue_graphics_08c33c14;
+extern struct CompressedGraphics wizards_waltz_prologue_graphics_08c33640;
 
 /* PROLOGUE (WIZARD'S WALTZ) - PALETTES */
 
