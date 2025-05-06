b43c29dab327d3fc3ae16cc8490652157dcc3443
arthurtilly <arthurtilly413@gmail.com>
2025-05-06 12:51:33 +1200

    marching orders

--- games/marching_orders/graphics/marching_orders_prologue_graphics.h
+++ games/marching_orders/graphics/marching_orders_prologue_graphics.h
@@ -9,9 +9,9 @@ extern struct Animation anim_marching_orders_prologue_title[];
 
 /* PROLOGUE (MARCHING ORDERS) - COMPRESSED GRAPHICS */
 
-extern struct CompressedGraphics marching_orders_prologue_graphics_08bfbe68;
-extern struct CompressedGraphics marching_orders_prologue_graphics_08bfc1f8;
-extern struct CompressedGraphics marching_orders_prologue_graphics_08bfa204;
+extern struct CompressedGraphics marching_orders_prologue_bg_tiles;
+extern struct CompressedGraphics marching_orders_prologue_bg_map;
+extern struct CompressedGraphics marching_orders_prologue_obj;
 
 /* PROLOGUE (MARCHING ORDERS) - PALETTES */
 
@@ -24,9 +24,9 @@ extern struct Animation anim_marching_orders_2_prologue_title[];
 
 /* PROLOGUE (MARCHING ORDERS 2) - COMPRESSED GRAPHICS */
 
-extern struct CompressedGraphics marching_orders_prologue_graphics_08bff238;
-extern struct CompressedGraphics marching_orders_prologue_graphics_08bff704;
-extern struct CompressedGraphics marching_orders_prologue_graphics_08bfc8f4;
+extern struct CompressedGraphics marching_orders_2_prologue_bg_tiles;
+extern struct CompressedGraphics marching_orders_2_prologue_bg_map;
+extern struct CompressedGraphics marching_orders_2_prologue_obj;
 
 /* PROLOGUE (MARCHING ORDERS 2) - PALETTES */
 
