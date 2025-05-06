8394023cc821c307c911b0fe6df252f3992b4c6e
arthurtilly <arthurtilly413@gmail.com>
2025-05-06 16:59:00 +1200

    move staff credit gfx tables

--- games/staff_credit/graphics/staff_credit_graphics.h
+++ games/staff_credit/graphics/staff_credit_graphics.h
@@ -6,3 +6,22 @@
 /* STAFF CREDIT - PALETTES */
 
 extern Palette staff_credit_bg_pal[];
+
+/* STAFF CREDIT - COMPRESSED GRAPHICS */
+
+extern struct CompressedGraphics staff_credit_graphics_08c3a8b0[];
+extern struct CompressedGraphics staff_credit_graphics_08c3f16c[];
+extern struct CompressedGraphics staff_credit_graphics_08c3b51c[];
+extern struct CompressedGraphics staff_credit_graphics_08c3f364[];
+extern struct CompressedGraphics staff_credit_graphics_08c3be64[];
+extern struct CompressedGraphics staff_credit_graphics_08c3f580[];
+extern struct CompressedGraphics staff_credit_graphics_08c3ca50[];
+extern struct CompressedGraphics staff_credit_graphics_08c3f798[];
+extern struct CompressedGraphics staff_credit_graphics_08c3d6f4[];
+extern struct CompressedGraphics staff_credit_graphics_08c3f9cc[];
+extern struct CompressedGraphics staff_credit_graphics_08c3e508[];
+extern struct CompressedGraphics staff_credit_graphics_08c3fc00[];
+extern struct CompressedGraphics staff_credit_graphics_08c3eb4c[];
+extern struct CompressedGraphics staff_credit_graphics_08c3fde4[];
+extern struct CompressedGraphics staff_credit_graphics_08c3ef78[];
+extern struct CompressedGraphics staff_credit_graphics_08c3fe9c[];
