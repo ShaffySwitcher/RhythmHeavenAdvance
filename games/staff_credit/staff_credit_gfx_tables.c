3c0ec1b94ba30df834b6997f037df946c84a7998
arthurtilly <arthurtilly413@gmail.com>
2025-05-06 16:59:00 +1200

    move staff credit gfx tables

---
+++ games/staff_credit/staff_credit_gfx_tables.c
@@ -0,0 +1,161 @@
+#include "engines/staff_credit.h"
+
+// [D_08a5dd8c] Graphics Table
+struct GraphicsTable D_08a5dd8c[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &staff_credit_graphics_08c3a8b0,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &staff_credit_graphics_08c3f16c,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Palette */ {
+        /* Src.  */ staff_credit_bg_pal[0],
+        /* Dest. */ BG_PALETTE_BUFFER(0),
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5ddbc] Graphics Table
+struct GraphicsTable D_08a5ddbc[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &staff_credit_graphics_08c3b51c,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &staff_credit_graphics_08c3f364,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Palette */ {
+        /* Src.  */ staff_credit_bg_pal[1],
+        /* Dest. */ BG_PALETTE_BUFFER(0),
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5ddec] Graphics Table
+struct GraphicsTable D_08a5ddec[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &staff_credit_graphics_08c3be64,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &staff_credit_graphics_08c3f580,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Palette */ {
+        /* Src.  */ staff_credit_bg_pal[2],
+        /* Dest. */ BG_PALETTE_BUFFER(0),
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5de1c] Graphics Table
+struct GraphicsTable D_08a5de1c[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &staff_credit_graphics_08c3ca50,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &staff_credit_graphics_08c3f798,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Palette */ {
+        /* Src.  */ staff_credit_bg_pal[3],
+        /* Dest. */ BG_PALETTE_BUFFER(0),
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5de4c] Graphics Table
+struct GraphicsTable D_08a5de4c[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &staff_credit_graphics_08c3d6f4,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &staff_credit_graphics_08c3f9cc,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Palette */ {
+        /* Src.  */ staff_credit_bg_pal[4],
+        /* Dest. */ BG_PALETTE_BUFFER(0),
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5de7c] Graphics Table
+struct GraphicsTable D_08a5de7c[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &staff_credit_graphics_08c3e508,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &staff_credit_graphics_08c3fc00,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Palette */ {
+        /* Src.  */ staff_credit_bg_pal[5],
+        /* Dest. */ BG_PALETTE_BUFFER(0),
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5deac] Graphics Table
+struct GraphicsTable D_08a5deac[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &staff_credit_graphics_08c3eb4c,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &staff_credit_graphics_08c3fde4,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Palette */ {
+        /* Src.  */ staff_credit_bg_pal[6],
+        /* Dest. */ BG_PALETTE_BUFFER(0),
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
+
+// [D_08a5dedc] Graphics Table
+struct GraphicsTable D_08a5dedc[] = {
+    /* BG Tileset */ {
+        /* Src.  */ &staff_credit_graphics_08c3ef78,
+        /* Dest. */ BG_TILESET_BASE(0),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Map */ {
+        /* Src.  */ &staff_credit_graphics_08c3fe9c,
+        /* Dest. */ BG_MAP_BASE(0xE800),
+        /* Size  */ COMPRESSED_GFX_SOURCE
+    },
+    /* BG Palette */ {
+        /* Src.  */ staff_credit_bg_pal[7],
+        /* Dest. */ BG_PALETTE_BUFFER(0),
+        /* Size  */ 0x140
+    },
+    END_OF_GRAPHICS_TABLE
+};
