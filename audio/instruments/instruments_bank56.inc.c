b58a45d99cdf82e3ee332ab6d60ae9368d2a7ad0
itaific <itaific@gmail.com>
2025-07-03 19:14:49 +0300

    huh?

--- audio/instruments/instruments_bank56.inc.c
+++ audio/instruments/instruments_bank56.inc.c
@@ -1117,13 +1117,18 @@ struct InstrumentPCM instrument_pcm_1553 = {
     /* ADSR Rel  */ 0x18EA90,
 };
 
+extern struct SampleData ninja_bodyguard_huh_data;
 extern struct SampleData sample_432_data;
 struct InstrumentPCM instrument_pcm_1554 = {
     /* Type      */ INSTRUMENT_PCM_ALIGNED,
     /* Key       */ 0x3C,
     /* Fast Read */ FALSE,
     /* Panning   */ 127,
+    #ifdef SFX
+    /* Sample    */ &ninja_bodyguard_huh_data,
+    #else
     /* Sample    */ &sample_432_data,
+    #endif
     /* ADSR Init */ 0x7F0000,
     /* ADSR Sus  */ 0x7F0000,
     /* ADSR Atk  */ 0x600000,
