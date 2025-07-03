6251a0ac347aba9f0b7aa7a360be08366b07e6bc
Shaffy <thedave2cz@gmail.com>
2025-07-03 15:29:07 +0200

    tap pa tap pa

--- src/engines/mechanical_horse.c
+++ src/engines/mechanical_horse.c
@@ -35,13 +35,35 @@ asm(".include \"include/gba.inc\""); // Temporary
 
 #include "asm/engines/mechanical_horse/asm_08041444.s"
 
-#include "asm/engines/mechanical_horse/asm_080415c0.s"
+void func_080415c0(u32 lesson) {
+    u32 i;
+    gMechanicalHorse->unk2cc = lesson;
+
+    sprite_set_anim(gSpriteHandler, gMechanicalHorse->horse[0].sprite, mechanical_horse_anim[lesson], 0, 0, 0, SPRITE_PLAYBACK_NORMAL_LOOP);
+    sprite_set_anim(gSpriteHandler, gMechanicalHorse->jockey[0].sprite, mechanical_horse_jockey_anim[lesson], 0, 0, 0, SPRITE_PLAYBACK_NORMAL_LOOP);
+
+    for(i = 0; i < 4; i++){
+        gMechanicalHorse->unk26c[i].pos_x = D_0805aab0[lesson][i] << 8;
+
+        sprite_set_x_y(gSpriteHandler, gMechanicalHorse->unk26c[i].sprite, gMechanicalHorse->unk26c[i].pos_x >> 8, gMechanicalHorse->unk26c[i].pos_y >> 8);
+        sprite_set_anim(gSpriteHandler, gMechanicalHorse->unk26c[i].sprite, mechanical_horse_lesson_text_anim[lesson][i], 1, 0, 0, SPRITE_PLAYBACK_NORMAL_LOOP);
+    }
+}
 
 #include "asm/engines/mechanical_horse/asm_080416cc.s"
 
 #include "asm/engines/mechanical_horse/asm_08041730.s"
 
-#include "asm/engines/mechanical_horse/asm_08041744.s"
+void func_08041744(u32 arg0) {
+    u8 i;
+    gMechanicalHorse->unk2e9 = arg0;
+    for (i = 0; i < 4; i++) {
+        sprite_set_visible(gSpriteHandler, gMechanicalHorse->unk26c[i].sprite, arg0);
+    }
+    if (arg0 == 0 && gMechanicalHorse->unk2cc == 3) {
+        gMechanicalHorse->unk2e9 = 1;
+    }
+}
 
 #include "asm/engines/mechanical_horse/asm_080417ac.s"
 
@@ -49,7 +71,84 @@ asm(".include \"include/gba.inc\""); // Temporary
 
 #include "asm/engines/mechanical_horse/asm_08041940.s"
 
-#include "asm/engines/mechanical_horse/asm_08041970.s"
+void func_08041970(void) {
+    u8 i;
+    u8 temp1;
+    u32 temp2;
+    for (i = 0; i < 20; i++) {
+        switch (gMechanicalHorse->unk3c[i].unk2) {
+            case 2:
+            case 3:
+                temp1 = FALSE;
+                if (gMechanicalHorse->unk3c[i].unk2 == 2) {
+                    temp1 = TRUE;
+                }
+
+                if (temp1) {
+                    gMechanicalHorse->unk3c[i].pos_x = gMechanicalHorse->horse[0].pos_x;
+                    gMechanicalHorse->unk3c[i].pos_z = INT_TO_FIXED(128.0078125);
+                } else {
+                    gMechanicalHorse->unk3c[i].pos_x = gMechanicalHorse->horse[1].pos_x;
+                    gMechanicalHorse->unk3c[i].pos_z = INT_TO_FIXED(128.01953125); //i hate this so much
+                }
+                gMechanicalHorse->unk3c[i].pos_y = INT_TO_FIXED(96);
+                gMechanicalHorse->unk3c[i].unk14[0] = D_0805aa40[gMechanicalHorse->unk2cc][0];
+                gMechanicalHorse->unk3c[i].unk14[1] = D_0805aa40[gMechanicalHorse->unk2cc][1];
+                gMechanicalHorse->unk3c[i].unk4 = 0;
+                if (gMechanicalHorse->unk2cc == 0) {
+                    if (gMechanicalHorse->unk3c[i].unk3 == 0 || gMechanicalHorse->unk3c[i].unk3 == 2) {
+                        sprite_set_anim(gSpriteHandler, gMechanicalHorse->unk3c[i].sprite, anim_horse_text_pak, 0, 0, 0, 0);
+                    } else {
+                        sprite_set_anim(gSpriteHandler, gMechanicalHorse->unk3c[i].sprite, anim_horse_text_ka, 0, 0, 0, 0);
+                    }
+                } else {
+                    if (gMechanicalHorse->unk2cc == 1) {     
+                        if (gMechanicalHorse->unk3c[i].unk3 == 0 || gMechanicalHorse->unk3c[i].unk3 == 2) {               
+                            sprite_set_anim(gSpriteHandler, gMechanicalHorse->unk3c[i].sprite, anim_horse_text_tot, 0, 0, 0, 0);
+                        } else {
+                            sprite_set_anim(gSpriteHandler, gMechanicalHorse->unk3c[i].sprite, anim_horse_text_ka, 0, 0, 0, 0);
+                        }
+                    } else if (gMechanicalHorse->unk2cc == 2) {
+                        if (gMechanicalHorse->unk3c[i].unk3 == 0) {
+                            sprite_set_anim(gSpriteHandler, gMechanicalHorse->unk3c[i].sprite, anim_horse_text_pa, 0, 0, 0, 0);
+                        } else if (gMechanicalHorse->unk3c[i].unk3 == 1) {
+                            sprite_set_anim(gSpriteHandler, gMechanicalHorse->unk3c[i].sprite, anim_horse_text_ka, 0, 0, 0, 0); 
+                        } else {
+                            sprite_set_anim(gSpriteHandler, gMechanicalHorse->unk3c[i].sprite, anim_horse_text_rap, 0, 0, 0, 0);
+                        }
+                    } else {
+                        if (gMechanicalHorse->unk3c[i].unk3 <= 2) {
+                             sprite_set_anim(gSpriteHandler, gMechanicalHorse->unk3c[i].sprite, anim_horse_text_do, 0, 0, 0, 0);
+                        } else {
+                             sprite_set_anim(gSpriteHandler, gMechanicalHorse->unk3c[i].sprite, anim_horse_text_dod, 0, 0, 0, 0);
+                        }
+                    }
+                }
+                sprite_set_x_y_z(gSpriteHandler, gMechanicalHorse->unk3c[i].sprite, FIXED_TO_INT(gMechanicalHorse->unk3c[i].pos_x), FIXED_TO_INT(gMechanicalHorse->unk3c[i].pos_y), gMechanicalHorse->unk3c[i].pos_z);
+                sprite_set_visible(gSpriteHandler, gMechanicalHorse->unk3c[i].sprite, 1);
+                gMechanicalHorse->unk3c[i].unk2 = 4;
+                break;
+            case 4:
+                gMechanicalHorse->unk3c[i].pos_x += gMechanicalHorse->unk3c[i].unk14[0];
+                gMechanicalHorse->unk3c[i].pos_y += gMechanicalHorse->unk3c[i].unk14[1];
+                gMechanicalHorse->unk3c[i].unk14[1] += INT_TO_FIXED(0.125); 
+                sprite_set_x_y(gSpriteHandler, gMechanicalHorse->unk3c[i].sprite, FIXED_TO_INT(gMechanicalHorse->unk3c[i].pos_x), FIXED_TO_INT(gMechanicalHorse->unk3c[i].pos_y));
+                if ((gMechanicalHorse->unk3c[i].pos_x < INT_TO_FIXED(-16)) || (gMechanicalHorse->unk3c[i].pos_y > INT_TO_FIXED(112))) {
+                    gMechanicalHorse->unk3c[i].unk2 = 0;
+                    sprite_set_visible(gSpriteHandler, gMechanicalHorse->unk3c[i].sprite, 0);
+                }
+                break;
+            case 1:
+                if (gMechanicalHorse->unk3c[i].unk4-- > 0) {
+                    break;
+                }
+                gMechanicalHorse->unk3c[i].unk2 = 3;
+                break;
+            case 0:
+                break;
+        }
+    }
+}
 
 #include "asm/engines/mechanical_horse/asm_08041c98.s"
 
