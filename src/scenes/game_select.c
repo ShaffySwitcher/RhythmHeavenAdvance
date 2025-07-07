efba17224a488538f580003e2f23ca0b20ca5a2c
itaific <itaific@gmail.com>
2025-07-07 15:02:33 +0300

    changing the "No practice" icon to look better + a fix to ninja bodyguard 2 to fix gf's blinking animation being seeminly reversed

--- src/scenes/game_select.c
+++ src/scenes/game_select.c
@@ -1846,7 +1846,7 @@ void game_select_init_info_pane(void) {
     text_printer_set_shadow_colors(gGameSelect->infoPaneDesc, -1);
     gGameSelect->perfectClearedSprite = sprite_create(gSpriteHandler, anim_game_select_perfect_rank, 0, 187, 112, 0x80A, 1, 0, 0x8000);
     sprite_set_origin_x_y(gSpriteHandler, gGameSelect->perfectClearedSprite, &bgOfs->x, &bgOfs->y);
-    gGameSelect->noPracticeSprite = sprite_create(gSpriteHandler, anim_game_select_no_practice, 0, 189, 94, 0x80A, 1, 0, 0x8000);
+    gGameSelect->noPracticeSprite = sprite_create(gSpriteHandler, anim_game_select_no_practice, 0, 188, 94, 0x80A, 1, 0, 0x8000);
     sprite_set_origin_x_y(gSpriteHandler, gGameSelect->noPracticeSprite, &bgOfs->x, &bgOfs->y);
     gGameSelect->infoPaneIsClear = TRUE;
     gGameSelect->infoPaneTask = INFO_PANE_TASK_NONE;
