6423079a38d69733f346d78f06013d322bbb86f9
itaific <itaific@gmail.com>
2025-07-13 15:18:20 +0300

    Karate Man 2 Prologue! (this came out so good)

---
+++ games/karate_man/graphics/karate_man_2_prologue_anim.c
@@ -0,0 +1,26 @@
+#include "global.h"
+#include "graphics.h"
+
+#include "karate_man_2_prologue_anim_cells.inc.c"
+
+// [D_088ada78] Title
+struct Animation anim_karate_man_2_prologue_title[] = {
+    /* 000 */ { karate_man_2_prologue_cel000, 20 },
+    /* 001 */ { karate_man_2_prologue_cel001, 10 },
+    /* 002 */ { karate_man_2_prologue_cel002, 10 },
+    /* 003 */ { karate_man_2_prologue_cel003, 10 },
+    /* 004 */ { karate_man_2_prologue_cel004, 10 },
+    /* 004 */ { karate_man_2_prologue_cel009, 50 },
+    /* End */ END_ANIMATION
+};
+
+// [D_088adaa8] Kick
+struct Animation anim_karate_man_2_prologue_kick[] = {
+    /* 000 */ { karate_man_2_prologue_cel007, 60 },
+    /* 001 */ { karate_man_2_prologue_cel005, 2 },
+    /* 002 */ { karate_man_2_prologue_cel006, 2 },
+    /* 003 */ { karate_man_2_prologue_cel005, 2 },
+    /* 004 */ { karate_man_2_prologue_cel006, 1 },
+    /* 005 */ { karate_man_2_prologue_cel005, 60 },
+    /* End */ END_ANIMATION
+};
---
+++ games/karate_man/graphics/karate_man_2_prologue_pal.c
@@ -0,0 +1,82 @@
+#include "global.h"
+#include "graphics.h"
+
+// [D_088adae0] OBJ Palette
+Palette karate_man_2_prologue_obj_pal[] = {
+    /* PALETTE 00 */ {
+        /* 00 */ TO_RGB555(0xC8E8F8),
+        /* 01 */ TO_RGB555(0x080000),
+        /* 02 */ TO_RGB555(0x27002e),
+        /* 03 */ TO_RGB555(0x470855),
+        /* 04 */ TO_RGB555(0x67107c),
+        /* 05 */ TO_RGB555(0x9117ab),
+        /* 06 */ TO_RGB555(0xc621e9),
+        /* 07 */ TO_RGB555(0xc621e9),
+        /* 08 */ TO_RGB555(0xc621e9),
+        /* 09 */ TO_RGB555(0x000000),
+        /* 10 */ TO_RGB555(0x000000),
+        /* 11 */ TO_RGB555(0x000000),
+        /* 12 */ TO_RGB555(0x000000),
+        /* 13 */ TO_RGB555(0x000000),
+        /* 14 */ TO_RGB555(0x000000),
+        /* 15 */ TO_RGB555(0xE0E800)
+    },
+    /* PALETTE 01 */ {
+        /* 00 */ TO_RGB555(0xB0D0E0),
+        /* 01 */ TO_RGB555(0x080000),
+        /* 02 */ TO_RGB555(0x271827),
+        /* 03 */ TO_RGB555(0x4b274e),
+        /* 04 */ TO_RGB555(0x633d6a),
+        /* 05 */ TO_RGB555(0x7e4f8c),
+        /* 06 */ TO_RGB555(0x888078),
+        /* 07 */ TO_RGB555(0xc621e9),
+        /* 08 */ TO_RGB555(0xc621e9),
+        /* 09 */ TO_RGB555(0xA09890),
+        /* 10 */ TO_RGB555(0xB8A8A0),
+        /* 11 */ TO_RGB555(0xC8C0B8),
+        /* 12 */ TO_RGB555(0xD8D8D8),
+        /* 13 */ TO_RGB555(0xE8E8E8),
+        /* 14 */ TO_RGB555(0xF0F0F0),
+        /* 15 */ TO_RGB555(0xF8F8F8)
+    }
+};
+
+// [D_088adb20] BG Palette
+Palette karate_man_2_prologue_bg_pal[] = {
+    /* PALETTE 00 */ {
+        /* 00 */ TO_RGB555(0xC8E8F8),
+        /* 01 */ TO_RGB555(0x080000),
+        /* 02 */ TO_RGB555(0x27002e),
+        /* 03 */ TO_RGB555(0x470855),
+        /* 04 */ TO_RGB555(0x67107c),
+        /* 05 */ TO_RGB555(0x9117ab),
+        /* 06 */ TO_RGB555(0xc621e9),
+        /* 07 */ TO_RGB555(0xc621e9),
+        /* 08 */ TO_RGB555(0xc621e9),
+        /* 09 */ TO_RGB555(0x000000),
+        /* 10 */ TO_RGB555(0x000000),
+        /* 11 */ TO_RGB555(0x000000),
+        /* 12 */ TO_RGB555(0x000000),
+        /* 13 */ TO_RGB555(0x000000),
+        /* 14 */ TO_RGB555(0x000000),
+        /* 15 */ TO_RGB555(0xE0E800)
+    },
+    /* PALETTE 01 */ {
+        /* 00 */ TO_RGB555(0xB0D0E0),
+        /* 01 */ TO_RGB555(0x080000),
+        /* 02 */ TO_RGB555(0x271827),
+        /* 03 */ TO_RGB555(0x4b274e),
+        /* 04 */ TO_RGB555(0x633d6a),
+        /* 05 */ TO_RGB555(0x7e4f8c),
+        /* 06 */ TO_RGB555(0x888078),
+        /* 07 */ TO_RGB555(0xc621e9),
+        /* 08 */ TO_RGB555(0xc621e9),
+        /* 09 */ TO_RGB555(0xA09890),
+        /* 10 */ TO_RGB555(0xB8A8A0),
+        /* 11 */ TO_RGB555(0xC8C0B8),
+        /* 12 */ TO_RGB555(0xD8D8D8),
+        /* 13 */ TO_RGB555(0xE8E8E8),
+        /* 14 */ TO_RGB555(0xF0F0F0),
+        /* 15 */ TO_RGB555(0xF8F8F8)
+    }
+};
