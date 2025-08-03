ace3837772016819e7a77109dc10985671204bb4
itaific <itaific@gmail.com>
2025-08-03 12:02:42 +0300

    still a bit slow but noticed the header was no longer RHYTHMADVANC and i kinda liked that header + made the "Press any button to continue." text like 2 pixels upwards so it wont look so awkward

--- src/scenes/warning.c
+++ src/scenes/warning.c
@@ -11,7 +11,7 @@ extern u8 D_03004498;
 
 // Render Animated Text (Script Function)
 void warning_scene_render_anim(void) {
-    sprite_create(gSpriteHandler, anim_warning_text_continue, 15, 120, 152, 0, 1, 0, 0);
+    sprite_create(gSpriteHandler, anim_warning_text_continue, 15, 120, 150, 0, 1, 0, 0);
 }
 
 
