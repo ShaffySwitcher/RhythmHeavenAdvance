e9e7541158003d4ef78968ed53b9618f5a5fa27d
itaific <itaific@gmail.com>
2025-10-07 00:09:27 +0300

    some more text and graphic changes mainly the fact WISH - Can't Wait For You got a special if case for the perfect notice not to get cut off (meaning the name is now back to having WISH in it)

--- src/scenes/game_select.c
+++ src/scenes/game_select.c
@@ -310,7 +310,6 @@ void start_campaign_notice(s32 id) {
     if (giftType == CAMPAIGN_GIFT_SONG) {
         isStandardSong = TRUE;
         switch (giftID) {
-            case STUDIO_SONG_HONEY_SWEET_ANGEL:
             case STUDIO_SONG_WISH:
                 isStandardSong = FALSE;
                 isSpecialSong = TRUE;
@@ -326,7 +325,11 @@ void start_campaign_notice(s32 id) {
     strcat(string, level->name); // "<game_name>"
     strcat(string, "\nright now, you'll earn:\n"); // Get a perfect on this
     strcat(string, ""); // "
+    if (!isSpecialSong) {
     strcat(string, get_campaign_gift_title(id, FALSE)); // "<gift>"
+    } else {
+        strcat(string, "WISH - Can't Wait\n for You");
+    }
     strcat(string, "\n"); // "
     if (isStandardSong) {
         strcat(string, "as a song."); // 's song
