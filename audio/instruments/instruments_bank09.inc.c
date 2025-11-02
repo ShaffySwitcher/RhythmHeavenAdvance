c1cb2f33c8f5ba46b8258b8f782454a58c1c60dd
itaific <itaific@gmail.com>
2025-11-02 11:52:49 +0200

    fixed the blue to yellow toss being cut off in remix 5 due to speed difference

--- audio/instruments/instruments_bank09.inc.c
+++ audio/instruments/instruments_bank09.inc.c
@@ -1080,6 +1080,21 @@ struct InstrumentPCM instrument_pcm_0242 = {
     /* ADSR Rel  */ 0x13E350,
 };
 
+extern struct SampleData sample_EN_NEW_2_data;
+struct InstrumentPCM instrument_pcm_2243 = {
+    /* Type      */ INSTRUMENT_PCM_ALIGNED,
+    /* Key       */ 0x3C,
+    /* Fast Read */ FALSE,
+    /* Panning   */ 127,
+    /* Sample    */ &sample_EN_NEW_2_data,
+    /* ADSR Init */ 0x7F0000,
+    /* ADSR Sus  */ 0x7F0000,
+    /* ADSR Atk  */ 0x600000,
+    /* ADSR Dec  */ 0x208000,
+    /* ADSR Fade */ 0x000000,
+    /* ADSR Rel  */ 0x13E350,
+};
+
 extern union Instrument inst_bank_54[];
 struct InstrumentSubRhythm instrument_rhy_0242 = {
     /* Type      */ INSTRUMENT_SUB_RHYTHM,
