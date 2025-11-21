d0de1b27c57063440a01946a0f4ec93149bc622b
itaific <itaific@gmail.com>
2025-11-21 13:05:37 +0200

    more text, more fixes and more fonts. but mainly a fix for the quiz show prologue to look better

--- games/quiz_show/graphics/quiz_show_prologue_anim.c
+++ games/quiz_show/graphics/quiz_show_prologue_anim.c
@@ -9,11 +9,11 @@ struct Animation anim_quiz_show_prologue_title[] = {
     /* 001 */ { quiz_show_prologue_cel001, 10 },
     /* 002 */ { quiz_show_prologue_cel002, 10 },
     /* 003 */ { quiz_show_prologue_cel003, 10 },
-    /* 004 */ { quiz_show_prologue_cel004, 10 },
-    /* 005 */ { quiz_show_prologue_cel005, 10 },
-    /* 006 */ { quiz_show_prologue_cel006, 10 },
-    /* 007 */ { quiz_show_prologue_cel007, 10 },
-    /* 008 */ { quiz_show_prologue_cel008, 10 },
-    /* 009 */ { quiz_show_prologue_cel009, 10 },
+    /* 004 */ { quiz_show_prologue_cel002, 10 },
+    /* 005 */ { quiz_show_prologue_cel001, 10 },
+    /* 006 */ { quiz_show_prologue_cel004, 10 },
+    /* 007 */ { quiz_show_prologue_cel005, 10 },
+    /* 008 */ { quiz_show_prologue_cel006, 10 },
+    /* 009 */ { quiz_show_prologue_cel007, 10 },
     /* End */ END_ANIMATION
 };
